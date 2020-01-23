a4f3cef225622024d1931b60bc7cff7f9fffe6c0
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-23 13:43:20 +0100

    :fire: add arrow function printOutput

--- src/app.ts
+++ src/app.ts
@@ -5,4 +5,7 @@ let age = 24;
 age = 23;
 
 const add = (a: number, b: number) => a + b;
-console.log(add(14, 10));
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+printOutput(add(14, 10));
